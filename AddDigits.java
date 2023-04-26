7206ed5039ff1733065ce7e0e25c81e9210ae9d4
vishwjeet thakur <ervishwjeetthakur@gmail.com>
2023-04-26 11:50:46 +0530

    one more approach added in AddDigits

--- AddDigits.java
+++ AddDigits.java
@@ -36,3 +36,13 @@ public class AddDigits {
         System.out.print(ans);
     }
 }
+
+//one  more better approach  Sc O(1)   Tc O(1)
+
+//class Solution {
+//    public int addDigits(int num) {
+//        if(num == 0) return 0;
+//        else if(num % 9 == 0) return 9;
+//        else return num % 9;
+//    }
+//}
