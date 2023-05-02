c31d479b61690949d562566955bdd68c02819503
vishwjeet thakur <ervishwjeetthakur@gmail.com>
2023-05-02 20:09:32 +0530

    Signoftheproductdone

---
+++ SignoftheProductofanArray.java
@@ -0,0 +1,19 @@
+package LeetcodeQue;
+
+public class SignoftheProductofanArray {
+    class Solution {
+        public int arraySign(int[] nums) {
+            int ans = 1;
+
+            for(int num : nums){
+                if(num==0){
+                    return 0;
+                }else if(num<0){
+                    ans = -ans;
+                }
+            }
+
+            return ans ;
+        }
+    }
+}
